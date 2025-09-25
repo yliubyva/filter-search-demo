8d20962778bf3c7eb77721d4039be2d7e3da6638
yliubyva <yliubyva@gmail.com>
2025-09-26 02:14:41 +0300

    feat: add sorting functionality to ProductList component

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,22 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { ProductItem } from '../ProductItem/ProductItem';
 import { Pagination } from '../Pagination/Pagination';
+import { ControlButton } from '../Buttons/ControlButton';
 import './ProductList.scss';
 
 export const ProductList = ({ data }) => {
   const [itemsPerPage, setItemsPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortKey, setSortKey] = useState('');
 
   useEffect(() => {
     setCurrentPage(1);
-  }, [itemsPerPage]);
+  }, [itemsPerPage, sortKey]);
 
-  const totalProducts = data.length;
+  const sortedProducts = useMemo(() => {
+    const sortedData = [...data];
+
+    if (sortKey === 'price' || sortKey === 'distance') {
+      sortedData.sort((a, b) => a[sortKey] - b[sortKey]);
+    } else if (sortKey === 'dateListed') {
+      sortedData.sort((a, b) => {
+        const dateA = new Date(a.dateListed).getTime();
+        const dateB = new Date(b.dateListed).getTime();
+        return dateB - dateA;
+      });
+    }
+
+    return sortedData;
+  }, [sortKey, data]);
+
+  const totalProducts = sortedProducts.length;
   const totalPages = Math.ceil(totalProducts / itemsPerPage);
   const indexLastProduct = currentPage * itemsPerPage;
   const indexFirstProduct = indexLastProduct - itemsPerPage;
 
-  const productsOnPage = data.slice(indexFirstProduct, indexLastProduct);
+  const productsOnPage = sortedProducts.slice(
+    indexFirstProduct,
+    indexLastProduct,
+  );
 
   const handleItemsPerPageChange = (event) => {
     const newItemsPerPage = Number(event.target.value);
@@ -32,8 +53,32 @@ export const ProductList = ({ data }) => {
       return newValue < min ? min : newValue;
     });
   };
+
+  const handleSortChange = (key) => {
+    setSortKey(key);
+  };
   return (
     <>
+      <div className="sort-bar">
+        <span className="sort-bar__label">Order by</span>
+        <div className="sort-bar__controls">
+          <ControlButton
+            label="lowest price"
+            isActive={sortKey === 'price'}
+            onClick={() => handleSortChange('price')}
+          />
+          <ControlButton
+            label="Closest"
+            isActive={sortKey === 'distance'}
+            onClick={() => handleSortChange('distance')}
+          />
+          <ControlButton
+            label="Newest Listings"
+            isActive={sortKey === 'dateListed'}
+            onClick={() => handleSortChange('dateListed')}
+          />
+        </div>
+      </div>
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
