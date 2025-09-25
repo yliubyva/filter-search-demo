3263dc837b8cc0b2e60040016b32ad37eec4cc7d
yliubyva <yliubyva@gmail.com>
2025-09-26 01:48:30 +0300

    feat: add pagination and related logic to ProductList component

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,18 +1,58 @@
+import { useEffect, useState } from 'react';
 import { ProductItem } from '../ProductItem/ProductItem';
+import { Pagination } from '../Pagination/Pagination';
 import './ProductList.scss';
 
 export const ProductList = ({ data }) => {
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [itemsPerPage]);
+
+  const totalProducts = data.length;
+  const totalPages = Math.ceil(totalProducts / itemsPerPage);
+  const indexLastProduct = currentPage * itemsPerPage;
+  const indexFirstProduct = indexLastProduct - itemsPerPage;
+
+  const productsOnPage = data.slice(indexFirstProduct, indexLastProduct);
+
+  const handleItemsPerPageChange = (event) => {
+    const newItemsPerPage = Number(event.target.value);
+    setItemsPerPage(newItemsPerPage > 0 ? newItemsPerPage : 1);
+  };
+
+  const handleStep = (direction) => {
+    setItemsPerPage((prevValue) => {
+      const step = 1;
+      const min = 1;
+      let newValue = direction === 'up' ? prevValue + step : prevValue - step;
+
+      return newValue < min ? min : newValue;
+    });
+  };
   return (
-    <ul className="product-list">
-      {data.map((product) => (
-        <ProductItem
-          key={product.id}
-          specification={product.specification}
-          title={product.title}
-          location={product.location}
-          price={product.price}
-        />
-      ))}
-    </ul>
+    <>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+        itemsPerPage={itemsPerPage}
+        handleItemsPerPageChange={handleItemsPerPageChange}
+        handleStep={handleStep}
+      />
+      <ul className="product-list">
+        {productsOnPage.map((product) => (
+          <ProductItem
+            key={product.id}
+            specification={product.specification}
+            title={product.title}
+            location={product.location}
+            price={product.price}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
