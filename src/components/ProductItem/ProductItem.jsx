2beb2f03ea6add9ad04c17f6cfb61eca735e5769
yliubyva <yliubyva@gmail.com>
2025-09-26 00:03:15 +0300

    feat: add utils/formatPrice and update existing component with formatted price and renamed prop

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,15 +1,16 @@
+import { formatPrice } from '../../utils/price';
 import './ProductItem.scss';
 
-export const ProductItem = ({ category, title, location, price }) => {
+export const ProductItem = ({ specification, title, location, price }) => {
   return (
     <li className="product-item">
       <article className="product-item__article">
         <div className="product-item__info">
-          <p className="product-item__category">{category}</p>
+          <p className="product-item__category">{specification}</p>
           <h3 className="product-item__title">{title}</h3>
           <p className="product-item__location">{location}</p>
         </div>
-        <span className="product-item__price">${price}</span>
+        <span className="product-item__price">{formatPrice(price)}</span>
       </article>
     </li>
   );
