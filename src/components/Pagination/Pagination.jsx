bdd96c0c979e3513f98927ea9e3165073f08e5a6
yliubyva <yliubyva@gmail.com>
2025-09-26 01:45:10 +0300

    feat: add Pagination component with styles

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,83 @@
+import { PaginationButton } from '../Buttons/PaginationButton';
+import './Pagination.scss';
+
+export const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  itemsPerPage,
+  handleItemsPerPageChange,
+  handleStep,
+}) => {
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1,
+  );
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="pagination">
+      <div className="pagination__controls">
+        <PaginationButton label="First" onClick={() => onPageChange(1)} />
+        <PaginationButton label="Prev" onClick={handlePrevPage} />
+
+        {pageNumbers.map((number) => (
+          <PaginationButton
+            key={number}
+            isActive={number === currentPage}
+            label={number}
+            onClick={() => onPageChange(number)}
+          />
+        ))}
+
+        <PaginationButton label="Next" onClick={handleNextPage} />
+        <PaginationButton
+          label="Last"
+          onClick={() => onPageChange(totalPages)}
+        />
+      </div>
+      <div className="results-per-page">
+        <label className="results-per-page__label">Results per page</label>
+        <div className="results-per-page__field">
+          <input
+            className="results-per-page__input"
+            type="number"
+            name="numberInput"
+            id="numberInput"
+            value={itemsPerPage}
+            onChange={handleItemsPerPageChange}
+            step={1}
+            min={1}
+          />
+          <div className="results-per-page__value-controls">
+            <button
+              className="results-per-page__value-control"
+              onClick={() => handleStep('up')}
+              title="Increase value"
+            >
+              ▲
+            </button>
+            <button
+              className="results-per-page__value-control"
+              onClick={() => handleStep('down')}
+              title="Decrease value"
+            >
+              ▼
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
