1684cf1ba62fd70ec2c51de5aaf3e3b5fe773340
yliubyva <yliubyva@gmail.com>
2025-09-26 17:11:10 +0300

    chore: update App component to include all components

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,24 @@
+import { CloseButton } from '../components/Buttons/CloseButton';
+import { DarkModeToggle } from '../components/DarkModeToggle/DarkModeToggle';
+import { productData } from '../data/ProductData';
+import { ProductList } from '../components/ProductList/ProductList';
+import './App.scss';
+
 function App() {
-  return <p>App</p>;
+  return (
+      <div className="app">
+        <header className="app__header">
+          <div className="app__top">
+            <h1>Filters</h1>
+            <CloseButton />
+          </div>
+          <DarkModeToggle />
+        </header>
+        <main>
+          <ProductList data={productData} />
+        </main>
+      </div>
+  );
 }
 
 export default App;
